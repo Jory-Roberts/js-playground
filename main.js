160038196c07f0910c3e93286279518ecfe8ac34
Jory-Roberts <robertsjory13@gmail.com>
2024-01-03 12:37:55 -0600

    adds index.html

--- main.js
+++ main.js
@@ -14,3 +14,10 @@ subtract(20, 30);
 const multiply = (num1, num2) => num1 * num2;
 
 multiply(24, 32);
+
+const divide = (num1, num2) => num1 / num2;
+
+divide(35, 7);
+
+let paragraphTag = document.getElementById('paragraph');
+console.log(paragraphTag);
