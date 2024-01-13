b0de384c8ad8a435c5989e27b068194832a948c0
Jory-Roberts <robertsjory13@gmail.com>
2024-01-13 10:16:31 -0500

    conditional statments

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const newDiv = document.createElement('myDiv');
-  newDiv.id = 'myDiv';
+let rating = 4;
 
-  document.body.appendChild(newDiv);
-  console.log(newDiv);
+if (rating === 3) {
+  console.log('UR AMAZING!!');
+} else if (rating === 2) {
+  console.log('MEETS EXPECTATIONS');
+} else if (rating === 1) {
+  console.log('U SUCK');
+} else {
+  console.log('INVALID');
+}
 
-  const newP = document.createElement('p');
-  newP.textContent = 'lorem epsum';
-  document.body.appendChild(newP);
-  console.log(newP);
-});
+let highScore = 1430;
+let userScore = 1200;
+
+if (userScore >= highScore) {
+  console.log(`Congrats, you have the new high score of ${userScore} `);
+} else {
+  console.log(`Good game. Your score of ${userScore} did not beat the high score of ${highScore}`);
+}
